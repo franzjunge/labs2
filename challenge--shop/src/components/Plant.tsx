52a379bb953886df80c07940adabcbf67b6c98f0
FJ <therealchitty+qa@gmail.com>
2025-06-18 11:47:47 +0100

    Completing challenge 2 - wired up add to cart button

--- challenge--shop/src/components/Plant.tsx
+++ challenge--shop/src/components/Plant.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type PlantProps = {
   id?: string,
   name: string,
@@ -6,13 +8,19 @@ type PlantProps = {
 }
 
 const Plant = ({name, price, imageUrl}: PlantProps) => {
+    const [addText, setAddText] = useState("Add to cart");
+    const [inCart, setInCart] = useState(false)
+    function addHandler(): void {
+      setAddText( inCart ? "In cart" : "Add to cart" );
+      setInCart(!inCart);
+    }
     return (
       <>
         <div>
           <img src={imageUrl} />
           <h3>{name}</h3>
           <p style={{fontSize: "small"}}>£{price}</p>
-          <small>TODO: Add to cart</small>
+          <button onClick={addHandler}>{addText}</button>
         </div>
       </>  
     );
